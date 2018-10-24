f28f9acd54de23f2c3105d53272820c549480538
Edmond Chuc <edmond.chuc@outlook.com>
2018-10-24 22:03:39 +1000

    Added login page

--- application.py
+++ application.py
@@ -8,11 +8,20 @@ app.config['SESSION_PERMANENT'] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route('/', methods=['GET'])
 def index():
-    if session.get('bucket') is None:
+    if session.get('name') is None:
         session['bucket'] = []
-    return render_template("index.html", bucket=session['bucket'])
+        return render_template("login.html")
+    else:
+        return render_template("index.html", bucket=session['bucket'])
+
+
+@app.route('/', methods=['POST'])
+def login():
+    session['name'] = request.form.get('name')
+    return render_template("index.html")
 
 
 @app.route('/add_item', methods=['POST'])
@@ -29,6 +38,12 @@ def clear():
     if session.get('bucket') is not None:
         session['bucket'][:] = []
     return render_template("index.html", bucket=session['bucket'])
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['name'] = None
+    return render_template("login.html")
 
 
 @app.route('/', defaults={'path': ''})
